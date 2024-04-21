914501c040d4fef656a6b4e866a764b406f67d2f
Ward <ceyssens.ward1@gmail.com>
2024-04-21 16:34:34 +0200

    basic upload field and response, ugly but functional

--- app/Django/views.py
+++ app/Django/views.py
@@ -2,8 +2,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django import forms
+
+
+class ImgForm(forms.Form):
+    img_field = forms.ImageField(widget=forms.FileInput)
+
+
 def index(request):
-    # Get an HttpRequest - the request parameter
-    # perform operations using information from the request.
-    # Return HttpResponse
-    return render(request, "../templates/index.html")
+    context = {}
+    context['imgForm'] = ImgForm()
+
+    if request.method == 'POST':
+        form = ImgForm(request.POST, request.FILES)
+        if form.is_valid():
+            img = form.cleaned_data['img_field']
+            # TODO: add ai here
+
+            # results is a list of tool names and confidence levels
+            context['results'] = []
+            context['results'].append({'tool': img, 'confidence': 0.9})
+            context['results'].append({'tool': 'a', 'confidence': 0.3})
+            context['results'].append({'tool': 'img', 'confidence': 0.001})
+
+    return render(request, "../templates/index.html", context)
